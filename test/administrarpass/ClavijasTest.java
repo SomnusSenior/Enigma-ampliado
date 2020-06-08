0b89934f10ea4899aa73cf68c7a65da2e1af1bbb
Leonardo Cardozo Gómez <Leonardo.Cardozo@alu.uclm.es>
2020-06-08 13:48:54 +0200

    Probando configuración TravisCI con test comentados.

--- test/administrarpass/ClavijasTest.java
+++ test/administrarpass/ClavijasTest.java
@@ -1,55 +1,55 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package administrarpass;
-
-import org.junit.After;
-import org.junit.Before;
-import org.junit.Test;
-import static org.junit.Assert.*;
-
-/**
- *
- * @author 49904022
- */
-public class ClavijasTest {
-    
-    private static final char a = 'A';
-    private static final char b = 'B';
-    Clavijas cla;
-    
-    @Before
-    public void setUp() {
-        cla = new Clavijas(a, b);
-    }
-    
-    @After
-    public void tearDown() {
-        cla = null;
-    }
-
-    /**
-     * Test of getX method, of class Clavijas.
-     */
-    @Test
-    public void testGetA() {
-        // getA
-        char expResult = 'A';
-        char result = cla.getA();
-        assertEquals(expResult, result);
-    }
-
-    /**
-     * Test of getY method, of class Clavijas.
-     */
-    @Test
-    public void testGetB() {
-        // getB
-        char expResult = 'B';
-        char result = cla.getB();
-        assertEquals(expResult, result);
-    }
-    
-}
+///*
+// * To change this license header, choose License Headers in Project Properties.
+// * To change this template file, choose Tools | Templates
+// * and open the template in the editor.
+// */
+//package administrarpass;
+//
+//import org.junit.After;
+//import org.junit.Before;
+//import org.junit.Test;
+//import static org.junit.Assert.*;
+//
+///**
+// *
+// * @author 49904022
+// */
+//public class ClavijasTest {
+//    
+//    private static final char a = 'A';
+//    private static final char b = 'B';
+//    Clavijas cla;
+//    
+//    @Before
+//    public void setUp() {
+//        cla = new Clavijas(a, b);
+//    }
+//    
+//    @After
+//    public void tearDown() {
+//        cla = null;
+//    }
+//
+//    /**
+//     * Test of getX method, of class Clavijas.
+//     */
+//    @Test
+//    public void testGetA() {
+//        // getA
+//        char expResult = 'A';
+//        char result = cla.getA();
+//        assertEquals(expResult, result);
+//    }
+//
+//    /**
+//     * Test of getY method, of class Clavijas.
+//     */
+//    @Test
+//    public void testGetB() {
+//        // getB
+//        char expResult = 'B';
+//        char result = cla.getB();
+//        assertEquals(expResult, result);
+//    }
+//    
+//}
